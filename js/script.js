3e647d86565efd6ed469dc594a258badb556fd19
João <Joo_Victor>
2023-09-30 16:29:14 -0300

    Aprofundando_Promisses

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 
 // Lidando com Fetch API
-
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/'); //Fetch é um parametro assincrono, que tem como parametro a URL da API 
+//?Fetch é um parametro assincrono, que tem como parametro a URL da API, em si, é uma PROMISSE
+var consultaCEP = fetch('https://viacep.com.br/ws/01000250/json/')
+    .then(resposta => resposta.json()) //ent transforme resposta em um arquivo json, POIS, os dados vem em formato de bytes e por isso não conseguiriamos ler eels
+    .then(r => {
+        if (r.erro) { //Aqui estamos tratando o erro. Pegando ele 
+            throw Error('Esse CEP não existe!') //?estamos declarando o erro que vai ser, para assim, o catch a seguir pegar esse erro
+        } else 
+            console.log(r)
+    })
+    //estamos pegando o valor de forma legivel                                                                                                 
+    //vamos tratar erros dessa requisição da API
+.catch(erro => console.log(erro))
+.finally(mensagem => console.log('Processamento concluido')); 
